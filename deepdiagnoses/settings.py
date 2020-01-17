6150230414d26f4e504fc81ccf29e952dd7451b9
NEHA SHUKLA <40213333+Neha-Shukla@users.noreply.github.com>
2020-01-17 10:50:01 +0530

    deleting id pass

--- deepdiagnoses/settings.py
+++ deepdiagnoses/settings.py
@@ -161,5 +161,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '9643kavinder@gmail.com'
-EMAIL_HOST_PASSWORD = '9643@Kavinder'
